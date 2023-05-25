50c40e253de0462d266d0261ad3ed93235ed2f58
Tony <84824688+Tony-Portfolio@users.noreply.github.com>
2023-05-25 14:09:34 +0000

    Update commit

---
+++ hotel/layouts/header.vue
@@ -0,0 +1,224 @@
+<template>
+    <div class="md:block hidden border-b-[1px] border-black/[0.1]">
+        <div class="bg-[#F7F7F7] py-5">
+            <div class="w-11/12 mx-auto flex items-center justify-between">
+                <h2 class="text-xl font-[500]">Kamar dan 50+ fitur terkini</h2>
+                <div class="flex gap-4 items-center">
+                    <img src="/images/video.jpg" alt="" class="w-[50px] rounded-lg">
+                    <p>lihat video kamar</p>
+                    <p class="">|</p>
+                    <p>Selengkapnya</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="sticky top-0 left-0 z-[100] bg-white shadow-md">
+        <div class="md:hidden block py-4">
+            <div
+                class="flex items-center justify-between w-11/12 border-[1px] border-black/[0.1] rounded-full mx-auto px-2 py-2 shadow-lg">
+                <div class="flex gap-4 items-center">
+                    <div class="flex items-center justify-center ml-2 font-bold">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                            class="w-6 h-6 font-bold">
+                            <path fill-rule="evenodd"
+                                d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div class="flex flex-col">
+                        <h3 class="text-[15px] font-bold">Kemana saja</h3>
+                        <p class="text-[13px] font-[500] text-black/[0.6]">Minggu mana pun , Tambahkan tamu</p>
+                    </div>
+                </div>
+                <div class="">
+                    <div
+                        class="h-[40px] w-[40px] border-[1px] border-black/[0.1] rounded-full flex items-center justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path
+                                d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="w-11/12 mx-auto flex flex-col gap-4 md:block hidden">
+            <div class="flex items-center justify-between py-4">
+                <div class="">
+                    <img src="/images/airbnb-logo.png" alt="" class="w-[100px]">
+                </div>
+                <div class="">
+                    <div class="">
+                        <ul
+                            class="flex border-2 border-black/[0.1] shadow-lg gap-4 py-2 px-3 rounded-full text-[14px] font-[500] items-center">
+                            <li class="pl-4">Ke mana saja</li>
+                            <li class="font-[400] text-black/[0.4]">|</li>
+                            <li>Minggu mana pun</li>
+                            <li class="font-[400] text-black/[0.4]">|</li>
+                            <li class="flex items-center gap-2">Tambahkan tamu <div
+                                    class="w-[35px] h-[35px] text-white rounded-full bg-[#FF385C] flex items-center justify-center p-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                        class="w-12 h-12">
+                                        <path fill-rule="evenodd"
+                                            d="M10.5 3.75a6.75 6.75 0 100 13.5 6.75 6.75 0 000-13.5zM2.25 10.5a8.25 8.25 0 1114.59 5.28l4.69 4.69a.75.75 0 11-1.06 1.06l-4.69-4.69A8.25 8.25 0 012.25 10.5z"
+                                            clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="">
+                    <p>Jadikan rumah anda airbnb</p>
+                </div>
+            </div>
+        </div>
+        <hr class="md:block hidden">
+        <div class="py-4">
+            <div class="w-11/12 mx-auto flex gap-4 items-center">
+                <div class="max-w-full overflow-hidden relative">
+                    <div
+                        class="p-2 bg-white border-[1px] border-black/[0.1] rounded-full text-sm absolute top-[50%] left-[2%] translate-x-[-50%] translate-y-[-50%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                            <path fill-rule="evenodd"
+                                d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <div
+                        class="p-2 bg-white border-[1px] border-black/[0.1] rounded-full text-sm absolute top-[50%] left-[98%] translate-x-[-50%] translate-y-[-50%]">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                            <path fill-rule="evenodd"
+                                d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                    <ul
+                        class="flex gap-2 gap-x-8 flex-nowrap font-[500] text-black/[0.6] text-[13px] overflow-x-auto text-center whitespace-nowrap">
+                        <li class="py-1 relative group">
+                            <img src="/icon/bed.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Kamar</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/tropis.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Tropis</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/beach.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Dekat pantai</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/rumah bumi.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Rumah bumi</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/golf.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Golf</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/view.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Pemandangan indah</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/camp.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Berkemah</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/kolam renang.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Kolam renang</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/modern.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Desain keren</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/rumah kecil.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Rumah kecil</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/taman nasional.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Taman nasional</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/mansion.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Mansion</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/wow.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Wow</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/luxe.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Luxe</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/hadap pantai.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Hadap pantai</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                        <li class="py-1 relative group">
+                            <img src="/icon/populer.jpg" alt="" class="w-[20px] mx-auto grayscale-[100%]">
+                            <p class="py-2">Populer</p>
+                            <div
+                                class="absolute bottom-0 left-0 w-full border-[1px] border-black/[0.1] opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="">
+                    <div
+                        class="border-[1px] border-black/[0.1] px-4 py-4 text-sm rounded-lg flex items-center justify-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path
+                                d="M18.75 12.75h1.5a.75.75 0 000-1.5h-1.5a.75.75 0 000 1.5zM12 6a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 6zM12 18a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 0112 18zM3.75 6.75h1.5a.75.75 0 100-1.5h-1.5a.75.75 0 000 1.5zM5.25 18.75h-1.5a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5zM3 12a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5A.75.75 0 013 12zM9 3.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5zM12.75 12a2.25 2.25 0 114.5 0 2.25 2.25 0 01-4.5 0zM9 15.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z" />
+                        </svg>
+                        <p>filter</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
---
+++ hotel/pages/hotel/[id].vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="">
+    <main class="w-10/12 mx-auto my-4" v-if="hotelData" :key="hotelData.id">
+      <p>{{ hotelData.id }}</p>
+    </main>
+  </div>
+</template>
+<script>
+import json from '@/assets/data/hotel.json';
+export default {
+  data() {
+    return {
+      layouts: {
+        header: "header",
+        footer: "footer"
+      },
+      hotel: json,
+    }
+  },
+  computed: {
+    hotelData() {
+      const itemId = this.$route.params.id; // Retrieve the ID from the URL
+      return this.hotel.find(item => item.id.toString() === itemId); // Find the matching item based on the ID
+    },
+  }
+}
+</script>
